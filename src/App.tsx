e5913da8250972f2b743d713e940f70765fdc58f
Flavio Andrade <foa12381@gmail.com>
2023-05-06 07:23:21 -0300

    config/vite

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
