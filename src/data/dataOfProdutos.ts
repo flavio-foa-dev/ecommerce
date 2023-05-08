8b69290f805e0b8e71f8b978a4fb156fa8b10033
Flavio Andrade <foa12381@gmail.com>
2023-05-08 12:38:54 -0300

    add data shoes

--- src/data/dataOfProdutos.ts
+++ src/data/dataOfProdutos.ts
@@ -1,275 +1,285 @@
-
+import {v4 as uuid} from 'uuid'
 
 export const shoes = [
   {
-    id: 1,
-    img: 'caminho/para/imagem1.jpg',
-    description: 'Sapato 1',
-    price: 99.99,
+    id: uuid(),
+    img: 'caminho/para/imagem1.jpg1',
+    description: 'Tênis Casual Plataforma Capricho - Branco',
+    price: 169.90,
     stock: 10,
     favorite: false,
     like: 15
   },
   {
-    id: 1,
-    img: 'caminho/para/imagem2.jpg',
-    description: 'Sapato 2',
+    id: uuid(),
+    img: 'caminho/para/imagem2.jpg2',
+    description: 'Tênis Adidas Coreracer - Preto',
+    price: 199.99,
+    stock: 5,
+    favorite: true,
+    like: 25
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem3.jpg3',
+    description: 'Tênis Casual Tribute Style Cano Baixo Skate',
+    price: 129.99,
+    stock: 7,
+    favorite: true,
+    like: 8
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem50.jpg4',
+    description: 'Tênis Adidas Masculino Futurepool 2.0 Branco Preto',
+    price: 649.99,
+    stock: 2,
+    favorite: false,
+    like: 12
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem1.jpg5',
+    description: 'Tênis Masculino Corrida - Preto',
+    price: 129.99,
+    stock: 10,
+    favorite: false,
+    like: 15
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem2.jpg6',
+    description: 'Tênis Qix 90S FutFanatics - Preto',
+    price: 279.99,
+    stock: 5,
+    favorite: true,
+    like: 25
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem3.jpg7',
+    description: 'Tênis Converse All Star - Branco',
+    price: 229.99,
+    stock: 7,
+    favorite: true,
+    like: 8
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem50.jpg8',
+    description: 'Tênis Adidas Pureboost - Preto',
+    price: 549.00,
+    stock: 2,
+    favorite: false,
+    like: 12
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem3.jpg9',
+    description: 'Tênis Caminhada tsx',
+    price: 129.99,
+    stock: 7,
+    favorite: true,
+    like: 8
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem50.jpg10',
+    description: 'Tênis Nike Air Max Sc Cw4555002 - Preto',
+    price: 949.99,
+    stock: 2,
+    favorite: false,
+    like: 12
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem1.jpg11',
+    description: 'Tênis Nike MC Trainer - Rosa',
+    price: 499.99,
+    stock: 10,
+    favorite: false,
+    like: 15
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem2.jpg12',
+    description: 'Sapatenis Sapatofran Esporte Fino com Relógio e Carteira',
+    price: 179.99,
+    stock: 5,
+    favorite: true,
+    like: 25
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem3.jpg13',
+    description: 'Botinha Molekinha Infantil - Rosa',
+    price: 129.99,
+    stock: 7,
+    favorite: true,
+    like: 8
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem50.jpg14',
+    description: 'Tênis Adidas Game Court - Verde',
+    price: 149.99,
+    stock: 2,
+    favorite: false,
+    like: 12
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem1.jpg15',
+    description: 'Tênis Havaianas TNS Mix',
+    price: 229.99,
+    stock: 10,
+    favorite: false,
+    like: 15
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem2.jpg16',
+    description: 'Tênis Asics Gel Rocket - Verde',
+    price: 579.99,
+    stock: 5,
+    favorite: true,
+    like: 25
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem2.jpg17',
+    description: 'Tênis xxxxxxxxxxxxxxxx',
+    price: 579.99,
+    stock: 5,
+    favorite: true,
+    like: 25
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem50.jpg19',
+    description: 'Sapato 50',
+    price: 149.99,
+    stock: 2,
+    favorite: false,
+    like: 12
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem3.jpg18',
+    description: 'Tênis Ortopédico Ortobela Comfort - Rosa',
+    price: 189.99,
+    stock: 7,
+    favorite: true,
+    like: 8
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem50.jpg19',
+    description: 'Tênis Nike MC Trainer - Rosa',
+    price: 449.99,
+    stock: 2,
+    favorite: false,
+    like: 12
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem3.jpg20',
+    description: 'Tênis Nike Kevin Durant Trey 5 IX - Preto',
+    price: 529.99,
+    stock: 7,
+    favorite: true,
+    like: 8
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem50.jpg21',
+    description: 'Tênis Ortopé Power Light Fecho Duplo - Azul',
+    price: 249.99,
+    stock: 2,
+    favorite: false,
+    like: 12
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem1.jpg22',
+    description: 'Tênis QIX2500 - vermelho',
+    price: 399.99,
+    stock: 10,
+    favorite: false,
+    like: 15
+  },
+  {
+    id: uuid(),
+    img: 'caminho/para/imagem2.jpg23',
+    description: 'Tênis Fila Start - Preto',
     price: 79.99,
     stock: 5,
     favorite: true,
     like: 25
   },
   {
-    id: 1,
-    img: 'caminho/para/imagem3.jpg',
-    description: 'Sapato 3',
-    price: 129.99,
+    id: uuid(),
+    img: 'caminho/para/imagem3.jpg24',
+    description: 'Tênis Nike Giannis Immortality - Cinza',
+    price: 929.99,
     stock: 7,
     favorite: true,
     like: 8
   },
   {
-    id: 1,
-    img: 'caminho/para/imagem50.jpg',
-    description: 'Sapato 50',
-    price: 149.99,
+    id: uuid(),
+    img: 'caminho/para/imagem50.jpg25',
+    description: 'Tênis beira Rio',
+    price: 89.99,
     stock: 2,
     favorite: false,
     like: 12
   },
   {
-    id: 1,
-    img: 'caminho/para/imagem1.jpg',
-    description: 'Sapato 1',
-    price: 99.99,
+    id: uuid(),
+    img: 'caminho/para/imagem1.jpg26',
+    description: 'Tênis Nike Air Zoom Pegasus - Preto',
+    price: 699.99,
     stock: 10,
     favorite: false,
     like: 15
   },
   {
-    id: 1,
-    img: 'caminho/para/imagem2.jpg',
-    description: 'Sapato 2',
-    price: 79.99,
+    id: uuid(),
+    img: 'caminho/para/imagem2.jpg27',
+    description: 'Tênis Wilson Kaos Stroke - Vermelho',
+    price: 879.99,
     stock: 5,
     favorite: true,
     like: 25
   },
   {
-    id: 1,
-    img: 'caminho/para/imagem3.jpg',
-    description: 'Sapato 3',
-    price: 129.99,
+    id: uuid(),
+    img: 'caminho/para/imagem3.jpg28',
+    description: 'Tênis Adidas CourtJam Control - Branco',
+    price: 729.99,
     stock: 7,
     favorite: true,
     like: 8
   },
   {
-    id: 1,
-    img: 'caminho/para/imagem50.jpg',
-    description: 'Sapato 50',
-    price: 149.99,
+    id: uuid(),
+    img: 'caminho/para/imagem50.jpg29',
+    description: 'Tênis Adidas Defiant Speed - Branco',
+    price: 849.99,
     stock: 2,
     favorite: false,
     like: 12
   },
   {
-    id: 1,
-    img: 'caminho/para/imagem3.jpg',
-    description: 'Sapato 3',
-    price: 129.99,
+    id: uuid(),
+    img: 'caminho/para/imagem3.jpg30',
+    description: 'Tênis Joma Ace Pro 2203 - Azul',
+    price: 729.99,
     stock: 7,
     favorite: true,
     like: 8
   },
-  {
-    id: 1,
-    img: 'caminho/para/imagem50.jpg',
-    description: 'Sapato 50',
-    price: 149.99,
-    stock: 2,
-    favorite: false,
-    like: 12
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem1.jpg',
-    description: 'Sapato 1',
-    price: 99.99,
-    stock: 10,
-    favorite: false,
-    like: 15
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem2.jpg',
-    description: 'Sapato 2',
-    price: 79.99,
-    stock: 5,
-    favorite: true,
-    like: 25
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem3.jpg',
-    description: 'Sapato 3',
-    price: 129.99,
-    stock: 7,
-    favorite: true,
-    like: 8
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem50.jpg',
-    description: 'Sapato 50',
-    price: 149.99,
-    stock: 2,
-    favorite: false,
-    like: 12
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem1.jpg',
-    description: 'Sapato 1',
-    price: 99.99,
-    stock: 10,
-    favorite: false,
-    like: 15
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem2.jpg',
-    description: 'Sapato 2',
-    price: 79.99,
-    stock: 5,
-    favorite: true,
-    like: 25
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem3.jpg',
-    description: 'Sapato 3',
-    price: 129.99,
-    stock: 7,
-    favorite: true,
-    like: 8
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem50.jpg',
-    description: 'Sapato 50',
-    price: 149.99,
-    stock: 2,
-    favorite: false,
-    like: 12
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem3.jpg',
-    description: 'Sapato 3',
-    price: 129.99,
-    stock: 7,
-    favorite: true,
-    like: 8
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem50.jpg',
-    description: 'Sapato 50',
-    price: 149.99,
-    stock: 2,
-    favorite: false,
-    like: 12
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem1.jpg',
-    description: 'Sapato 1',
-    price: 99.99,
-    stock: 10,
-    favorite: false,
-    like: 15
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem2.jpg',
-    description: 'Sapato 2',
-    price: 79.99,
-    stock: 5,
-    favorite: true,
-    like: 25
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem3.jpg',
-    description: 'Sapato 3',
-    price: 129.99,
-    stock: 7,
-    favorite: true,
-    like: 8
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem50.jpg',
-    description: 'Sapato 50',
-    price: 149.99,
-    stock: 2,
-    favorite: false,
-    like: 12
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem1.jpg',
-    description: 'Sapato 1',
-    price: 99.99,
-    stock: 10,
-    favorite: false,
-    like: 15
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem2.jpg',
-    description: 'Sapato 2',
-    price: 79.99,
-    stock: 5,
-    favorite: true,
-    like: 25
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem3.jpg',
-    description: 'Sapato 3',
-    price: 129.99,
-    stock: 7,
-    favorite: true,
-    like: 8
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem50.jpg',
-    description: 'Sapato 50',
-    price: 149.99,
-    stock: 2,
-    favorite: false,
-    like: 12
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem3.jpg',
-    description: 'Sapato 3',
-    price: 129.99,
-    stock: 7,
-    favorite: true,
-    like: 8
-  },
-  {
-    id: 1,
-    img: 'caminho/para/imagem50.jpg',
-    description: 'Sapato 50',
-    price: 149.99,
-    stock: 2,
-    favorite: false,
-    like: 12
-  },
+
 ];
 
